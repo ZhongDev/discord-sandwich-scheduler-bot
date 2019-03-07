553f5ee800c5864cffd85ed4b370f2edff8c2935
ZhongDev <jason@desuu.com>
2019-03-07 15:04:28 +1100

    Add blacklist and Reroll

--- dssb.js
+++ dssb.js
@@ -6,6 +6,7 @@ var schedule = require('node-schedule')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const Keyv = require('keyv')
+const moment = require('moment')
 const dssbDB = new Keyv('sqlite://' + __dirname + '/dssbDB.sqlite', { namespace: 'dssb' })
 const blacklistDB = new Keyv('sqlite://' + __dirname + '/dssbDB.sqlite', { namespace: 'blacklist' })
 blacklistDB.on('error', (err) => { console.error(err) })
@@ -34,6 +35,85 @@ client.on('message', message => {
 
         case `${PREFIX}whatsmyid`:
             message.reply('Your discord author id is ' + messageauthor)
+            return
+
+        case `${PREFIX}reroll`:
+            if (isGuildOwner) {
+                dssbDB.get('channels')
+                    .then((channels) => {
+                        if (channels == undefined) {
+                            var channelobj = {}
+                        } else {
+                            var channelobj = JSON.parse(channels)
+                        }
+                        if (channelobj[message.guild.id] == undefined) {
+                            channelobj[message.guild.id] = {}
+                        }
+                        if (channelobj[message.guild.id].tommorowssandwich == undefined){
+                            message.reply('Please set the tommorows-sandwich channel with `!setchannel tomorrows-sandwich`')
+                            return
+                        }
+                        dssbDB.get('choices')
+                            .then((choices) => {
+                                if (choices == undefined) {
+                                    var choicesobj = {}
+                                } else {
+                                    var choicesobj = JSON.parse(choices)
+                                }
+                                if (choicesobj[message.guild.id] == undefined) {
+                                    choicesobj[message.guild.id] = []
+                                }
+                                if (choicesobj[message.guild.id].length < 1){
+                                    message.reply('Please add a choice to pick from')
+                                    return
+                                }
+                                dssbDB.get('balance')
+                                    .then((balance) => {
+                                        if (balance == undefined) {
+                                            var balanceobj = {}
+                                        } else {
+                                            var balanceobj = JSON.parse(balance)
+                                        }
+                                        if (balanceobj[message.guild.id] == undefined) {
+                                            balanceobj[message.guild.id] = 0
+                                        }
+                                        if(balanceobj[message.guild.id] <= 0 ){
+                                            message.reply('You have no balance. Please add balance to roll.')
+                                            return
+                                        }
+                                        var choicesCanPayFor = choicesobj[message.guild.id].filter(choice => choice.cost <= balanceobj[message.guild.id])
+                                        if(choicesCanPayFor.length < 1){
+                                            message.reply('You don\'t have enough balance to pay for any choices. Please add balance enough balance to roll.')
+                                            return
+                                        }
+                                        var choicesNotBlacklisted = choicesCanPayFor.filter(choice => choice.blacklist - moment().unix()*1000 <= 0)
+                                        if(choicesNotBlacklisted.length < 1){
+                                            message.reply('Everything you can pay for has been blacklisted. Please either add balance so that a non-blacklisted item can be purchased, or remove items from the blacklist.')
+                                            return
+                                        }
+                                        var selected = choicesNotBlacklisted[Math.floor(Math.random() * choicesNotBlacklisted.length)]
+                                        message.reply('The sandwich that has been rolled is ...')
+                                        setTimeout(function(){
+                                            console.log(selected)
+                                            message.reply(selected.sandwich + " ($"+ selected.cost/100 + ")")
+                                            client.channels.get(channelobj[message.guild.id].tommorowssandwich).fetchMessages()
+                                                .then((list)=>{
+                                                    return client.channels.get(channelobj[message.guild.id].tommorowssandwich).bulkDelete(list);
+                                                })
+                                                .then(() => {
+                                                    client.channels.get(channelobj[message.guild.id].tommorowssandwich).send('selected.sandwich + " ($"+ selected.cost/100 + ")"')
+                                                })
+                                                .catch((err) => {
+                                                    console.err(err)
+                                                    message.reply('Something went wrong while attempting to send the message to the tommorows-sandwich channel')
+                                                })
+                                        },1500)
+                                    })
+                            })
+                    })
+            } else {
+                message.reply('Only the server owner can use this command.')
+            }
             return
 
         case `${PREFIX}setchannel`:
@@ -184,21 +264,43 @@ client.on('message', message => {
                                     return sandwichObj.sandwich === sandwich
                                 })
                                 if(alreadyExist){
-                                    message.reply('A sandwich with the same name already exists. Please remove it first to readd it.')
+                                    message.reply('A sandwich with the same name already exists. Please remove it first to re-add it.')
                                     return
                                 }
                                 choicesobj[message.guild.id].push({
                                     sandwich,
-                                    cost: choiceCostCents
+                                    cost: choiceCostCents,
+                                    blacklist: 0
                                 })
                                 dssbDB.set('choices', JSON.stringify(choicesobj))
                                 message.reply(sandwich + ' with the price of $' + choiceCostCents / 100 + ' has been successfully added to the list of choices.')
                                 var choicelist = []
-                                choicesobj[message.guild.id].forEach(choice => {
-                                    choicelist.push('`' + choice.sandwich + '` `$' + choice.cost/100 + '`')
-                                });
-                                var choicelistStr = choicelist.join('\n')
-                                message.reply('Current Choices are :\n' + choicelistStr)
+                                dssbDB.get('balance')
+                                    .then((balance) => {
+                                        if (balance == undefined) {
+                                            var balanceobj = {}
+                                        } else {
+                                            var balanceobj = JSON.parse(balance)
+                                        }
+                                        if (balanceobj[message.guild.id] == undefined) {
+                                            balanceobj[message.guild.id] = 0
+                                        }
+                                        choicesobj[message.guild.id].forEach(choice => {
+                                            var blacklisted = ''
+                                            var cantAfford = ''
+                                            if(choice.blacklist - moment().unix()*1000 > 0){
+                                                var time = moment(choice.blacklist).fromNow()
+                                                blacklisted = ' `BLACKLISTED (expires ' + time + ')`'
+                                            }
+                                            if(choice.cost > balanceobj[message.guild.id]){
+                                                var cantAfford = ' --Cannot afford'
+                                            }
+                                            var choiceStr = '`' + choice.sandwich + '` `$' + choice.cost/100 + '` ' + cantAfford + blacklisted
+                                            choicelist.push(choiceStr.trim())
+                                        });
+                                        var choicelistStr = choicelist.join('\n')
+                                        message.reply('Current Choices are :\n' + choicelistStr)
+                                    })
                             })
                     }else{
                         message.reply('The money value provided does not seem to be valid. Please try again.')
@@ -237,11 +339,32 @@ client.on('message', message => {
                             dssbDB.set('choices', JSON.stringify(choicesobj))
                             message.reply(sandwich + ' has been removed from the list of choices.')
                             var choicelist = []
-                            choicesobj[message.guild.id].forEach(choice => {
-                                choicelist.push('`' + choice.sandwich + '` `$' + choice.cost/100 + '`')
-                            });
-                            var choicelistStr = choicelist.join('\n')
-                            message.reply('Current Choices are :\n' + choicelistStr)
+                            dssbDB.get('balance')
+                                .then((balance) => {
+                                    if (balance == undefined) {
+                                        var balanceobj = {}
+                                    } else {
+                                        var balanceobj = JSON.parse(balance)
+                                    }
+                                    if (balanceobj[message.guild.id] == undefined) {
+                                        balanceobj[message.guild.id] = 0
+                                    }
+                                    choicesobj[message.guild.id].forEach(choice => {
+                                        var blacklisted = ''
+                                        var cantAfford = ''
+                                        if(choice.blacklist - moment().unix()*1000 > 0){
+                                            var time = moment(choice.blacklist).fromNow()
+                                            blacklisted = ' `BLACKLISTED (expires ' + time + ')`'
+                                        }
+                                        if(choice.cost > balanceobj[message.guild.id]){
+                                            var cantAfford = ' --Cannot afford'
+                                        }
+                                        var choiceStr = '`' + choice.sandwich + '` `$' + choice.cost/100 + '` ' + cantAfford + blacklisted
+                                        choicelist.push(choiceStr.trim())
+                                    });
+                                    var choicelistStr = choicelist.join('\n')
+                                    message.reply('Current Choices are :\n' + choicelistStr)
+                                })
                         })
                 }
             }else{
@@ -261,13 +384,88 @@ client.on('message', message => {
                         choicesobj[message.guild.id] = []
                     }
                     var choicelist = []
-                    choicesobj[message.guild.id].forEach(choice => {
-                        choicelist.push('`' + choice.sandwich + '` `$' + choice.cost/100 + '`')
-                    });
-                    var choicelistStr = choicelist.join('\n')
-                    message.reply('Current Choices are :\n' + choicelistStr)
+                    dssbDB.get('balance')
+                        .then((balance) => {
+                            if (balance == undefined) {
+                                var balanceobj = {}
+                            } else {
+                                var balanceobj = JSON.parse(balance)
+                            }
+                            if (balanceobj[message.guild.id] == undefined) {
+                                balanceobj[message.guild.id] = 0
+                            }
+                            choicesobj[message.guild.id].forEach(choice => {
+                                var blacklisted = ''
+                                var cantAfford = ''
+                                if(choice.blacklist - moment().unix()*1000 > 0){
+                                    var time = moment(choice.blacklist).fromNow()
+                                    blacklisted = ' `BLACKLISTED (expires ' + time + ')`'
+                                }
+                                if(choice.cost > balanceobj[message.guild.id]){
+                                    var cantAfford = ' --Cannot afford'
+                                }
+                                var choiceStr = '`' + choice.sandwich + '` `$' + choice.cost/100 + '` ' + cantAfford + blacklisted
+                                choicelist.push(choiceStr.trim())
+                            });
+                            var choicelistStr = choicelist.join('\n')
+                            message.reply('Current Choices are :\n' + choicelistStr)
+                        })
                 })
             break
+
+        case `${PREFIX}blacklistchoice`:
+            if (isGuildOwner) {
+                if (arguments.length < 2){
+                    message.reply('To blacklist a choice please use `' + PREFIX + 'blacklistchoice <choice> <amount of days>`')
+                    return
+                }
+                var currentMillisecondsEpoch = moment().unix()*1000
+                var days = arguments[arguments.length - 1]
+                var blacklistEpoch = currentMillisecondsEpoch + days * 24 * 60 * 60 * 1000;
+                arguments.pop()
+                var sandwich = arguments.join(' ')
+                if (isNaN(days)) {
+                    message.reply('Please provide the amount of days as a number')
+                    return
+                }
+                message.reply('Blacklisting ' + sandwich + ' for ' + days + ' days')
+                dssbDB.get('choices')
+                    .then((choices) => {
+                        if (choices == undefined) {
+                            var choicesobj = {}
+                        } else {
+                            var choicesobj = JSON.parse(choices)
+                        }
+                        if (choicesobj[message.guild.id] == undefined) {
+                            choicesobj[message.guild.id] = []
+                        }
+                        var alreadyExist = choicesobj[message.guild.id].some(sandwichObj => {
+                            return sandwichObj.sandwich === sandwich
+                        })
+                        if(!alreadyExist){
+                            message.reply('The specified sanwich does not exist in the choice list. Please check your capilization and try again.')
+                            return
+                        }
+                        choicesobj[message.guild.id] = choicesobj[message.guild.id].map(obj => obj.sandwich === sandwich ? { ...obj, blacklist: blacklistEpoch } : obj)
+                        dssbDB.set('choices', JSON.stringify(choicesobj))
+                        message.reply('Successfully blacklisted ' + sandwich + ' for ' + days + ' days')
+                        choicesobj[message.guild.id] = choicesobj[message.guild.id].filter(obj => obj.blacklist !== 0)
+                        var choicelist = []
+                        choicesobj[message.guild.id].forEach(choice => {
+                            timeTillBlacklist = choice.blacklist - currentMillisecondsEpoch
+                            if(timeTillBlacklist > 0){
+                                var time = moment(choice.blacklist).fromNow()
+                                choicelist.push('`' + choice.sandwich + '` expires `' + time + '`')
+                            }
+                        });
+                        var choicelistStr = choicelist.join('\n')
+                        message.reply('The blacklist is currently:\n' + choicelistStr)
+                    })
+            } else {
+                message.reply('Only the server owner can use this command.')
+            }
+            break
+
     }
 });
 
